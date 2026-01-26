58ad888e442566dafc56607f3f013bf3c4dd114e
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-26 16:36:52 +0530

    Code in logger.js file added Done

---
+++ src/config/logger.js
@@ -0,0 +1,64 @@
+const winston = require('winston');
+const path = require('path');
+
+const logLevels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const logColors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'blue',
+};
+
+winston.addColors(logColors);
+
+const logFormat = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.errors({ stack: true }),
+  winston.format.splat(),
+  winston.format.json()
+);
+
+const consoleFormat = winston.format.combine(
+  winston.format.colorize({ all: true }),
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}${info.stack ? '\n' + info.stack : ''}`
+  )
+);
+
+const transports = [
+  new winston.transports.Console({
+    format: consoleFormat,
+  }),
+  new winston.transports.File({
+    filename: path.join(__dirname, '../../logs/error.log'),
+    level: 'error',
+    format: logFormat,
+    maxsize: 5242880,
+    maxFiles: 5,
+  }),
+  new winston.transports.File({
+    filename: path.join(__dirname, '../../logs/combined.log'),
+    format: logFormat,
+    maxsize: 5242880,
+    maxFiles: 5,
+  }),
+];
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  levels: logLevels,
+  format: logFormat,
+  transports,
+  exitOnError: false,
+});
+
+module.exports = logger; 
